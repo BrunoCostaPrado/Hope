e83a8d465b2e41a21f1091ab4018664d2e926842
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-02 00:08:20 -0300

    Update Hope.py

--- Hope.py
+++ Hope.py
@@ -3,11 +3,13 @@ import json
 import pickle
 import numpy as np
 import nltk
+
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
-lemmatizer = WordNetLemmatizer
+lemma = WordNetLemmatizer()
 intents = json.loads(open('Hope/intents.json').read())
+
 
 words = pickle.load(open('words.plk', 'rb'))
 classes = pickle.load(open('classes.plk', 'rb'))
@@ -16,18 +18,18 @@ model = load_model('Hope_model.model')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = [lemma.lemmatize(word) for word in sentence_words]
     return sentence_words
 
 
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
-    bag = [0]*len(words)
+    bag = [0] * len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
-                return np.array(bag)
+    return np.array(bag)
 
 
 def predict_class(sentence):
@@ -39,4 +41,23 @@ def predict_class(sentence):
     return_list = []
     for r in result:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-        return return_list
+    return return_list
+
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+print("Foi! Bot funcionando")
+
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
